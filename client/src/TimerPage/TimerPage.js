6b385b48b966c2b80e3bfd4b512dc52bf331d381
adi-levy1 <adi.levy1995@gmail.com>
2021-06-03 23:59:30 +0300

    bla

--- client/src/TimerPage/TimerPage.js
+++ client/src/TimerPage/TimerPage.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "./TimerPage.css";
 import {MdKeyboardArrowRight} from "react-icons/md";
 import {TiArrowBack} from "react-icons/ti";
+import {BiShareAlt} from "react-icons/bi";
+import {AiOutlineClose} from "react-icons/ai";
+import Timer from "./Timer";
+
 class TimerPage extends Component {
     state = {
         company: this.props.company ? this.props.company : "",
@@ -30,12 +34,41 @@ class TimerPage extends Component {
             }
         ],
     };
+    calculateTimeLeft = () => {
+        let year = new Date().getFullYear();
+        let difference = +new Date(`10/01/${year}`) - +new Date();
+        let timeLeft = {};
 
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        }
+
+        return timeLeft;
+
+    }
     render() {
         return (
             <div>
-             <MdKeyboardArrowRight className="MdArrow"/>
-             <TiArrowBack className="BArrow" />
+             <Timer  hoursMinSecs={{days:1,hours:0, minutes: 0, seconds: 3}}/>
+                <div className="s">
+                    שניות
+                </div>
+                <div className="m">
+                    דקות
+                </div>
+                <div className="h">
+                    שעות
+                </div>
+                <div className="d">
+                    ימים
+                </div>
+             <BiShareAlt className="MdArrow"/>
+             <AiOutlineClose className="BArrow" />
 
               <div className="bet">
                     התערבות פתוחה
@@ -44,12 +77,12 @@ class TimerPage extends Component {
                 כמה זמן ייקח לשקד לסיים לסיים את כל העונות של משחקי הכס?
               </div>
               <div className="total">
-                 450$
+                  ₪450
                   <br/>
                   סך הכל בקופה
               </div>
                 <div className="participant">
-                    50$
+                    ₪50
                     <br/>
                     סכום למשתתף
                 </div>
