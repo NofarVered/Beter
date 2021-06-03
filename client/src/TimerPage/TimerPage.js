d49883489018fd4bb09f8952a642f9abb3cd9ae7
Noam-Yair <noam0906@gmail.com>
2021-06-03 18:51:41 +0300

    added routing

--- client/src/TimerPage/TimerPage.js
+++ client/src/TimerPage/TimerPage.js
@@ -7,12 +7,34 @@ class TimerPage extends Component {
         betText: this.props.betText,
         amount: this.props.amount,
         timer: "01:09:10:02",
+        participants: [{
+            name: "נעם",
+            answer: "חמישה ימים בלחץ",
+        },
+            {
+                name: "עדי",
+                answer: "שלושה ימים",
+            },
+            {
+                name: "נופר",
+                answer: "יומיים",
+            },
+            {
+                name: "עדן",
+                answer: "שלושה ימים",
+            },
+            {
+                name: "מעיין",
+                answer: "ארבעה ימים",
+            }
+        ],
     };
+
 
     render() {
         return (
             <div className="">
-
+                Noam
             </div>
 
 
