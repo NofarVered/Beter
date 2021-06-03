42d1e4f2285e4d776c875f0a38c6b217c2491521
adi-levy1 <adi.levy1995@gmail.com>
2021-06-04 02:05:00 +0300

    added answer

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Home from "./Home/Home";
 import TimerPage from "./TimerPage/TimerPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BetAnswer from "./BetAnswer/BetAnswer";
 
 function App() {
   return (
       <Router>
-    <div className="App">
+    <div className="App" dir="rtl">
         <Route
             exact path="/home">
             <Home/>
@@ -16,7 +17,10 @@ function App() {
                 exact path="/timerPage">
                 <TimerPage/>
             </Route>
-
+        <Route
+            exact path="/BetAnswer">
+            <BetAnswer/>
+        </Route>
 
     </div>
       </Router>
