6b9a25069b0198b5e5b70d48d0722795bc681b69
adi-levy1 <adi.levy1995@gmail.com>
2021-06-03 17:28:02 +0300

    public/private

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -6,12 +6,38 @@ import Plus from "./plus.png";
 
 class Home extends Component {
     state = {
-        name: this.props.name,
+        border_pr: "0px solid",
+        fWeight_pr : "normal",
+        border_pu: "0px solid",
+        fWeight_pu : "normal",
     };
+    private = (e) => {
+        this.setState({
+            border_pr: "3px solid",
+            fWeight_pr : "bold",
+            border_pu: "0px solid",
+            fWeight_pu : "normal",
+        })
+    }
+    public = (e) => {
+        this.setState({
+            border_pu: "3px solid",
+            fWeight_pu : "bold",
+            border_pr: "0px solid",
+            fWeight_pr : "normal",
+        })
+    }
 
     render() {
+
         return (
             <div>
+                <div className="head">
+                    <h1 className="title">לוח התערבויות</h1>
+                    <div className="private" onClick={this.private}  style={{  fontWeight:this.state.fWeight_pr, borderBottom: this.state.border_pr}}>פרטי</div>
+                    <div className="public" onClick={this.public} style={{  fontWeight:this.state.fWeight_pu, borderBottom: this.state.border_pu}}> ציבורי </div>
+
+                </div>
                 <div className="nav-bar">
                     <img className="nav-button" src={Coupon}/>
                     <img className="nav-plus" src={Plus}/>
